e8e050a3b4588e6992ecb8562170fc9d9d6322e7
mosesalphonse <mosesalphonse>
2021-12-08 13:35:55 +0000

    v1 snapshot

--- src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -18,7 +18,7 @@ public class GreetingResourceTest {
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
-                .body(is("hello v4"));
+                .body(is("hello v1"));
     }
 
     @Test
@@ -29,7 +29,7 @@ public class GreetingResourceTest {
                 .when().get("/hello/greeting/{name}")
                 .then()
                 .statusCode(200)
-                .body(is("hello v4 " + uuid));
+                .body(is("hello v1 " + uuid));
     }
 
 }
